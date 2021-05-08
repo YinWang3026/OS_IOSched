c25b38ecf8fc89aee31b39d7328b7dd225db3558
Yin Wang <yw2178@nyu.edu>
2021-05-08 19:36:08 -0400

    start of sim

--- iosched.cpp
+++ iosched.cpp
@@ -15,21 +15,35 @@ int vFlag = 0;
 int qFlag = 0;
 int fFlag = 0;
 
+// Macros
+#define vtrace(fmt...)  do { if (vFlag) { printf(fmt); fflush(stdout); } } while(0)
+#define qtrace(fmt...)  do { if (qFlag) { printf(fmt); fflush(stdout); } } while(0)
+#define ftrace(fmt...)  do { if (fFlag) { printf(fmt); fflush(stdout); } } while(0)
+
 // Struct
 struct IORequest {
-    IORequest(int t, int tr) : time(t), track(tr) {}
+    static int count;
+    IORequest(int at, int tr) : id(count++), arrival_time(at), track(tr), service_start(-1), service_end(-1){}
     void print(){
-        printf("time[%d], track[%d]\n", time, track); 
+        printf("id[%d], time[%d], track[%d]\n", id, arrival_time, track); 
     }
-    int time;
+    void print_sol(){
+        printf("%5d: %5d %5d %5d\n",id, arrival_time, service_start, service_end);
+    }
+    int id;
+    int arrival_time;
     int track;
+    int service_start;
+    int service_end;
 };
+int IORequest::count = 0;
 
 // Class definitions
 class IOScheduler {
 public:
     virtual ~IOScheduler() {}
 private:
+    queue<IORequest*> IOQueue;
 };
 
 class FIFO : public IOScheduler{
@@ -53,7 +67,7 @@ class FLOOK : public IOScheduler{
 };
 
 // Prototype functions
-void simulation(IOScheduler*, queue<IORequest>&);
+int simulation(IOScheduler*, vector<IORequest*>&);
 
 int main(int argc, char* argv[]) {
     int c;
@@ -108,42 +122,67 @@ int main(int argc, char* argv[]) {
     //Opening input file
     ifstream ifs(inputFile);
     if (!ifs) {
-        cerr << "Error: Could not open the rfile.\n";
+        cerr << "Error: Could not open input file.\n";
         exit(1);
     }
     int time, track;
     string line;
-    queue<IORequest> requestQueue;
+    vector<IORequest*> requestVector;
     while (getline(ifs, line)) {
         if (line[0] == '#') {
             continue;
         }
         istringstream iss(line);
         iss >> time >> track;
-        requestQueue.push(IORequest(time,track));
+        requestVector.push_back(new IORequest(time,track));
     }
     ifs.close();
+    if (requestVector.empty()) {
+        cerr << "No requests were given." << endl;
+        exit(1);
+    }
 
     // Simulation
-    simulation(myIOSched, requestQueue);
+    int tot_movement = simulation(myIOSched, requestVector);
+
+    // Results 
+    int total_time = 0;
+    double avg_turnaround = 0;
+    double avg_waittime = 0;
+    int max_waittime = 0;
+    for (size_t i = 0; i < requestVector.size(); i++){
+        IORequest* req = requestVector[i];
+        req->print_sol();
+        total_time = max(total_time, req->service_end);
+        avg_turnaround += (req->service_end - req->arrival_time);
+        int wait_time = req->service_start - req->arrival_time;
+        avg_waittime += wait_time;
+        max_waittime = max(max_waittime, wait_time);
+    }
+    avg_turnaround /= requestVector.size();
+    avg_waittime /= requestVector.size();
+    printf("SUM: %d %d %.2lf %.2lf %d\n",total_time, tot_movement, avg_turnaround, avg_waittime, max_waittime);
 
     // Clean up
     delete myIOSched;
+    for (size_t i = 0; i < requestVector.size(); i++){
+        delete requestVector[i];
+    }
 }
 
-void simulation(IOScheduler* myIOSched, queue<IORequest>& requestQueue){
+int simulation(IOScheduler* myIOSched, vector<IORequest*>& requestVector){
+    int n = requestVector.size();
+    int tot_movement = 0;
     int currentTime = 0;
-    while (!requestQueue.empty()){
-        IORequest temp = requestQueue.front();
-        temp.print();
-        requestQueue.pop();
+    int currentRequestIndex = 0;
+    int track = 0; // 1 track = 1 time
+    IORequest* currentRequest = NULL;
+   
+
+    while (false) {
+
+        currentTime += 1;
     }
 
-    // while (true) {
-    //     if () {
-
-    //     } else {
-    //         currentTime += 1;
-    //     }
-    // }
+    return tot_movement;
 }
